960e3b38e6da5513d5f8d10b2dae13e338af5c92
Matt C <matt.cheah@gmail.com>
2017-09-08 22:14:20 -0700

    add some commands and make sure not to miss out on merge => trinity

--- commands.js
+++ commands.js
@@ -11,8 +11,12 @@ module.exports = {
   commit: 'save', // Commit your code to the Lord and it will be saved.
   push: 'preach', // Proudly declare your Gospel code to those who need it.
   rebase: 'disciple', // Regularly examine your code, words and actions and compare them with the Word of God.
-  merge: 'fellowship', // Have fellowship with other's code that it may become one body.
+  merge: 'trinity', // Father, Son, and Holy Spirit. Three in One. 
   checkout: 'resurrect', // Resurrect what was once alive.
   log: 'testimony', // Use your code's testimony to share the path the Lord has led you along.
-  pull: 'petition' // Petition the Lord through prayer to grant you those remote code changes.
+  pull: 'petition', // Petition the Lord through prayer to grant you those remote code changes.
+  reset: 'ark',     // God hit reset on the earth. 
+  branch: 'vine',   // He is the vine, we are the branches. 
+  init: 'inthebeginning', // Start us off, God. 
+  tag: 'testament', // God's way of semantic versioning.    	
 };
