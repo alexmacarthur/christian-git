904d9b0bc72583cccd28da82fcaa4745937a28d1
Alex MacArthur <alex@macarthur.me>
2021-05-05 20:47:27 -0500

    Update dependencies, small JS tweak.

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 const { spawnSync } = require("child_process");
 const { getCommandByAlias, colorLog } = require("./helpers");
 
-let completeCommand = process.argv;
-let gitCommand = getCommandByAlias(completeCommand[2]);
+const completeCommand = process.argv;
+const gitCommand = getCommandByAlias(completeCommand[2]);
 
 completeCommand.splice(0, 3, gitCommand);
 
